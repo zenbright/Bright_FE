bb296f9f126075a7675360f26c2bf360c2b66047
quietfriend03 <97797248+quietfriend03@users.noreply.github.com>
2023-11-10 16:51:46 +0700

    Revert "Development"

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,28 @@
-
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-import ButtonText from "./components/general/buttonText";
-
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [trigger, setTrigger] = useState(false);
+  const [count, setCount] = useState(0)
 
-  function trueOrFalse(type) {
-    setTrigger(!trigger);
-    console.log(type);
-  }
   return (
     <>
-
-      <div>
-        <ButtonText
-          shape="rounded-2xl"
-          backgroundColor="green-500"
-          text="oke nha"
-          onSelected={() => trueOrFalse("aloooo")}
-        />
-        <p>{trigger ? "alo" : "alo2"} text</p>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
+      <p className="text-3xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
-  );
+  )
 }
 
-export default App;
-
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-// import './App.css';
-
-// function ButtonText() {
-//   const [count1, setCount1] = useState(false);
-
-//   function setBoolean() {
-//     setCount1(!count1);
-//     console.log("alo");
-//   }
-
-//   return (
-//     <div className="buttonText">
-//       <button onClick={setBoolean}>
-//         {count1 ? <span>alo</span> : <span>alo2</span>} text1
-//       </button>
-//     </div>
-//   );
-// }
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <ButtonText />
-
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount(count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   );
-// }
-
-// export default App;
+export default App
