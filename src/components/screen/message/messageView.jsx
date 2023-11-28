d2d458a7e38647c94788df7b136012dd36c0503c
Mudoker <huuquoc7603@gmail.com>
2023-11-28 21:44:20 +0700

    Fix scroll view + custom scrollbar

--- src/components/screen/message/messageView.jsx
+++ src/components/screen/message/messageView.jsx
@@ -9,29 +9,33 @@ export const MessageView = () => {
 
     const MessageList = () => {
         const list = Array.from({ length: 10 }, (_, i) => (
-            <Message key={i} onClick={() => setSelectedMessage(i)} isSelected={selectedMessage == i ? true : false} sentTime={'12:00 PM'} userName={`Mudoker + ${i}`} profileImage={image} message={'Helloooooooooooooooooooooooooooooooooo'} />
+            <Message key={i} onClick={() => setSelectedMessage(i)} isSelected={selectedMessage === i} sentTime={'12:00 PM'} userName={`Mudoker + ${i}`} profileImage={image} message={'Helloooooooooooooooooooooooooooooooooo'} />
         ));
         return list;
     };
 
     return (
-        <div className="flex">
+        <div className="flex h-screen">
             {/* Nav bar */}
-            <div className="w-1/12 h-100 bg-black"/>
+            <div className="w-1/12 h-screen bg-black" />
 
-            <div className="flex-col flex-shrink-0 border-r">
-                <div className="text-3xl font-medium p-4 flex items-center justify-between">
+            {/* Message section */}
+            <div className="flex-col border-r h-screen">
+                <div className="text-3xl font-medium p-4 flex items-center justify-between h-20">
                     <h1>Message</h1>
                     <button type="button" className="w-6 h-6">
                         <img src={messageNew} alt="writing" />
                     </button>
                 </div>
 
-                {/* Message list */}
-                <MessageList />
+                {/* Make the message list scrollable */}
+                <div className="overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-200" style={{ maxHeight: 'calc(100vh - 80px)' }}>
+                    <MessageList />
+                </div>
             </div>
 
-            <div className="flex w-screen items-center justify-center h-screen">
+            {/* Message Content */}
+            <div className="flex w-screen items-center justify-center">
                 <MessageContent selectedMessage={selectedMessage} />
             </div>
         </div>
