acb4285af243a2c56e33f0134df5b0fff6f74f88
Mudoker <huuquoc7603@gmail.com>
2024-02-03 00:10:40 +0700

    refactor: fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import {Button} from '@/components/ui/button';
-
 import React from 'react';
 
 
@@ -9,7 +7,9 @@ function App() {
       <h1 style={{fontFamily: 'nunito', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
       <h1 style={{fontFamily: 'nunito-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
       <h1 style={{fontFamily: 'nunitosans', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
-      <h1 style={{fontFamily: 'nunitosans-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+      <h1
+        style={{fontFamily: 'nunitosans-italic', fontSize: 50, alignSelf: 'center'}}
+      >Hello World</h1>
     </div>
   );
 }
