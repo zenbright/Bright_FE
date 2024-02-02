a0c9a8066bf80b0c3c9e9612f342ae2d4ba510c7
Danglhp <dangcongly06@gmail.com>
2024-02-02 23:53:38 +0700

    refactor: add custom fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import {Button} from '@/components/ui/button';
 
 import React from 'react';
 
+
 function App() {
   return (
     <div>
-      <Button>Click me</Button>
+        <h1 style={{fontFamily: 'nunito', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+        <h1 style={{fontFamily: 'nunito-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+        <h1 style={{fontFamily: 'nunitosans', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+        <h1 style={{fontFamily: 'nunitosans-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
     </div>
   );
 }
