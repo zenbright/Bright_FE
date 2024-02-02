8653a5fe79f78531074860efd42a40afcd03a244
Mudoker <huuquoc7603@gmail.com>
2024-02-03 00:10:07 +0700

    refactor: font.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import React from 'react';
 function App() {
   return (
     <div>
-        <h1 style={{fontFamily: 'nunito', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
-        <h1 style={{fontFamily: 'nunito-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
-        <h1 style={{fontFamily: 'nunitosans', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
-        <h1 style={{fontFamily: 'nunitosans-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+      <h1 style={{fontFamily: 'nunito', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+      <h1 style={{fontFamily: 'nunito-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+      <h1 style={{fontFamily: 'nunitosans', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
+      <h1 style={{fontFamily: 'nunitosans-italic', fontSize: 50, alignSelf: 'center'}}>Hello World</h1>
     </div>
   );
 }
