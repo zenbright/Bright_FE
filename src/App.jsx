07171d6ee0d7bbef8401e71b7de44d76c71f02fa
Danglhp <dangcongly06@gmail.com>
2024-02-03 15:36:24 +0700

    add sign up

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import {Button} from '@/components/ui/button';
 
 import React from 'react';
 import Loginview from './components/login/Loginview';
+import Signupview from './components/signup/Signup';
+
 function App() {
   return (
-    <Loginview />
+    //<Loginview />
+    <Signupview />
   );
 }
 
---
+++ src/components/signup/component/button/googleButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from "../../../ui/button"
+import { Mail } from "lucide-react"
+
+function GoogleButton ({onClick}) {
+    return (
+        <Button 
+                onClick={onClick}
+                className=" w-full h-9 rounded px-5 py-2.5 
+                text-black text-sm bg-white
+                font-medium hover:bg-gray-300
+                text-center inline-flex items-center mb-2 border border-gray-400">
+            <Mail className="mr-2 h-4 w-4" />
+                Google 
+        </Button>
+    )
+}
+
+export default GoogleButton
