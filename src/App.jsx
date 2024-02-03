71169496a0995341678c1c7642020ac3ef6b009c
Danglhp <dangcongly06@gmail.com>
2024-02-03 17:02:08 +0700

    update sign up

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import {Button} from '@/components/ui/button';
 import React from 'react';
 import Loginview from './components/login/Loginview';
 import Signupview from './components/signup/Signup';
-
+import SignupButton from './components/signup/component/button/SignupButton';
 function App() {
   return (
     //<Loginview />
     <Signupview />
+    //<SignupButton />
   );
 }
 
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -26,7 +26,7 @@ const Signupview = () => {
                 <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r'>
                     <div className='absolute inset-0' style={divStyle} />
                     <div className='relative z-20 flex items-center text-lg font-medium'>
-                        <img src={logo} alt='logo' className='object-contain w-32' />
+                        <img src={logo} alt='logo' className='object-contain w-40' />
 
                     </div>
                     <div className='relative z-20 mt-auto'>
