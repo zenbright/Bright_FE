c443f98c202209261cc1598b9f1bde06f1b9c27c
Mudoker <huuquoc7603@gmail.com>
2024-02-03 13:24:58 +0700

    update divier

--- src/features/dashboard/components/dashboardPage.jsx
+++ src/features/dashboard/components/dashboardPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ProjectBreadCrumbs from './breadcrumbs';
+import HorizontalDivider from '../../../components/general/divider';
 
-export const Page = ({title = 'MUDOKER'}) => {
+export const Page = () => {
   return (
-    <div className='ml-3'>
-      <ProjectBreadCrumbs projectType='SOFTWARE' projectOwner='MUDOKER' />
+    <div>
+      <div className='ml-3'>
+        <ProjectBreadCrumbs projectType='SOFTWARE' projectOwner='MUDOKER' />
+
+        <h1 className='text-2xl font-bold text-slate-700 mb-2' style={{fontFamily: 'nunito'}}
+        >
+        Bright
+        </h1>
+      </div>
+
+      <HorizontalDivider height='0.75px'/>
     </div>
   );
 };
