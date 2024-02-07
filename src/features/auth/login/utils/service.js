cb89aa8aee29e7fd1ae15e85784279f7913c910a
DarkChan <buihohaidang@gmail.com>
2024-02-08 00:25:45 +0700

    refactor: update google button

--- src/features/auth/login/utils/service.js
+++ src/features/auth/login/utils/service.js
@@ -1,8 +1,8 @@
-import { axiosPost } from "@/config/service/axios.js";
+import {axiosPost} from '@/config/service/axios.js';
 
 const login = async (account, password) => {
   try {
-    const response = await axiosPost("bright-backend/api/auth/bright/login", {
+    const response = await axiosPost('bright-backend/api/auth/bright/login', {
       account,
       password,
     });
@@ -11,29 +11,5 @@ const login = async (account, password) => {
     console.error(error);
   }
 };
-
-const getCookie = (name) => {
-  const nameEQ = name + '=';
-  const ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-};
-
-const setCookie = (name, value, days) => {
-  let expires = '';
-  if (days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = '; expires=' + date.toUTCString();
-  }
-  document.cookie = name + '=' + (value || '') + expires + '; path=/';
-};
-
-// Get the access token from the cookie
-const accessToken = getCookie('accessToken');
 
 export default login;
