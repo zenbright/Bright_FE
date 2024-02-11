f07f2c3d6cf66bada485fef08c373eedf34f309a
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-02-11 20:45:30 +0700

    feat!: display the default message when any message is received - not the custom values

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,3 +5,27 @@ Please don't delete the file.
 REFERENCE
 https://firebase.google.com/docs/cloud-messaging/js/send-multiple
 */
+
+// When a notification is received, the push event is called.
+self.addEventListener(
+  "push",
+  function (event) {
+    console.log("event: " + JSON.stringify(event));
+    
+    let messageTitle = "Bright";
+    let messageBody = "Check your notifications on the app!";
+
+    const notificationPromise = self.registration.showNotification(
+      messageTitle,
+      {
+        body: messageBody,
+      }
+    );
+
+    event.waitUntil(notificationPromise);
+  },
+  false
+);
+
+
+
--- src/features/auth/login/getDeviceToken.js
+++ src/features/auth/login/getDeviceToken.js
@@ -11,10 +11,21 @@ const app = initializeApp(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG));
 // // Get a reference to the messaging service
 const messaging = getMessaging(app);
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered');
+    })
+    .catch((err) => {
+      console.error('Service Worker registration failed:', err);
+    });
+}
+
 export async function getDeviceToken() {
   try {
     const currentToken = await getToken(messaging, {
-      vapidKey: import.meta.env.VITE_VAPIDKEY,
+      // vapidKey: import.meta.env.VITE_VAPIDKEY,
+      vapidKey: import.meta.env.VITE_VAPIDKEY
     });
 
     if (currentToken) {
