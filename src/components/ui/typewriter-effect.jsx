fcbd5a45397d0dd9a1b5fc9c558f146832e09a42
Mudoker <huuquoc7603@gmail.com>
2024-02-29 19:52:43 +0700

    fix: fix type writer animation

--- src/components/ui/typewriter-effect.jsx
+++ src/components/ui/typewriter-effect.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import {cn} from '@/lib/utils';
 import {motion, useAnimation, useInView} from 'framer-motion';
+import {useRef} from 'react';
 import {useState} from 'react';
 import {useEffect} from 'react';
 
@@ -83,25 +84,6 @@ export const TypewriterEffectSmooth = ({
     text: word.text.split(''),
   }));
 
-  console.log(words);
-  const renderWords = () => (
-    <div>
-      {wordsArray.map((word, idx) => (
-        <div key={`word-${idx}`} className="inline-block">
-          {word.text.map((char, index) => (
-            <span
-              key={`char-${index}`}
-              className={`dark:text-white text-black ${word.className} text-black dark:text-blue-500`}
-            >
-              {char}
-            </span>
-          ))}
-          &nbsp;
-        </div>
-      ))}
-    </div>
-  );
-
   const [animationKey, setAnimationKey] = useState(0);
 
   useEffect(() => {
@@ -112,6 +94,28 @@ export const TypewriterEffectSmooth = ({
     return () => clearTimeout(timer);
   }, [animationKey]);
 
+  const renderWords = () => {
+    return (
+      <div>
+        {wordsArray.map((word, idx) => (
+          <div key={`word-${idx}`} className="inline-block">
+            {word.text.map((char, index) => (
+              <span
+                key={`char-${index}`}
+                className={`dark:text-white text-black ${word.className}`}
+                style={{color: word.className ? '#fbbf24': 'black'}}
+              >
+                {char}
+              </span>
+            ))}
+            {idx !== words.length - 1 && <>&nbsp;</>}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+
   return (
     <div className={cn('flex space-x-1 my-6', className)}>
       <motion.div
@@ -120,7 +124,8 @@ export const TypewriterEffectSmooth = ({
         initial={{
           width: '0%',
         }}
-        animate={{
+        whileInView={{
+          width: '265px',
           width: 'fit-content',
         }}
         transition={{
@@ -135,8 +140,8 @@ export const TypewriterEffectSmooth = ({
             whiteSpace: 'nowrap',
           }}
         >
-          {renderWords()}{' '}
-        </div>{' '}
+          {renderWords()}
+        </div>
       </motion.div>
       <motion.span
         initial={{
