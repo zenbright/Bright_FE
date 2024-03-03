26a323ff92a03f4eca78ee558a1fbc5041db1606
Mudoker <huuquoc7603@gmail.com>
2024-03-03 16:28:45 +0700

    fix: update imports

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from '../../components/sidebar';
+import Sidebar from '../components/sidebar';
 import {Outlet} from 'react-router-dom';
 
 export const AppLayout = () => {
