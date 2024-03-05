706fe86c56e93d2fb3a6aa550166c87f1e5c02c5
@S52100903 <52100903@student.tdtu.edu.vn>
2024-03-05 21:39:12 +0700

    fix: reformat the structure base on eslint

--- src/features/setting/component/setting-tab.jsx
+++ src/features/setting/component/setting-tab.jsx
@@ -1,30 +1,29 @@
-import { useState } from "react"
 import PropTypes from 'prop-types';
 
 function SettingTab({tab, tabIndex, selectedActive, handleSelected}) {
-    return (
-        <div className={`flex flex-col p-3 w-full group cursor-pointer border-slate-300 border-b-[1px] hover:bg-slate-100
+  return (
+    <div className={`flex flex-col p-3 w-full group cursor-pointer border-slate-300 border-b-[1px] hover:bg-slate-100
                         ${selectedActive === tabIndex ? 'border-r-blue-700 rounded-none border-r-[3px]' : ''}`}
-            onClick={handleSelected}
-        >
-            <div className={`flex flex-row gap-2 
+    onClick={handleSelected}
+    >
+      <div className={`flex flex-row gap-2 
                             ${selectedActive === tabIndex ? 'text-blue-700' : ''}`}
-            >
-                {tab.icon}
-                <h1 className='font-bold text-lg group-hover:text-blue-700'>{tab.name}</h1>
-            </div>
-            <div className='ml-8'>
-                <p>{tab.paragraph}</p>
-            </div>
-        </div>
-    )
+      >
+        {tab.icon}
+        <h1 className='font-bold text-lg group-hover:text-blue-700'>{tab.name}</h1>
+      </div>
+      <div className='ml-8'>
+        <p>{tab.paragraph}</p>
+      </div>
+    </div>
+  );
 }
 
 SettingTab.propTypes = {
-    tab: PropTypes.node,
-    tabIndex: PropTypes.number,
-    selectedActive: PropTypes.number,
-    handleSelected: PropTypes.func
-}
+  tab: PropTypes.node,
+  tabIndex: PropTypes.number,
+  selectedActive: PropTypes.number,
+  handleSelected: PropTypes.func,
+};
 
-export default SettingTab
+export default SettingTab;
