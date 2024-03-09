94b596ab4ba2949d6b734979b4420c43cfc8189a
Mudoker <huuquoc7603@gmail.com>
2024-03-09 19:13:09 +0700

    refactor: update view ui

--- src/features/setting/component/account-page.jsx
+++ src/features/setting/component/account-page.jsx
@@ -23,6 +23,7 @@ import {zodResolver} from '@hookform/resolvers/zod';
 import {useForm} from 'react-hook-form';
 import {z} from 'zod';
 import {ACCOUNT} from '../test/data/strings';
+import {OverlayScrollbarsComponent} from 'overlayscrollbars-react';
 
 
 const formShcema = z.object({
@@ -61,115 +62,119 @@ function Account() {
   };
 
   return (
-    <div className="container-ns flex flex-col w-[75vw] overflow-auto">
-      <div className="z-30 mx-3 text-lg font-bold top-0 p-2 border-b-[1px] border-slate-300 group sticky bg-white">
-        {'Account'}
+
+    <OverlayScrollbarsComponent>
+
+      <div className="container-ns flex flex-col w-[75vw] overflow-auto">
+        <div className="z-30 mx-3 text-lg font-bold top-0 p-2 border-b-[1px] border-slate-300 group sticky bg-white">
+          {'Account'}
+        </div>
+
+        <div className="flex flex-col">
+          <div className="m-3 border-b-[1px] p-2">
+            <p className="font-semibold">{'Change Password'}</p>
+          </div>
+
+          <div className="mx-3 p-2 space-y-4">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit({onSubmit, onError})}>
+                {/* To support accesibility */}
+                <input hidden type="text" autoComplete="username" />
+
+                <FormField
+                  control={form.control}
+                  name="old_password"
+                  render={({field}) => (
+                    <FormItem className="flex flex-col">
+                      <div className="flex flex-row items-center justify-between w-[40vw] mt-2">
+                        <FormLabel className="font-semibold text-base">{'Old password'}</FormLabel>
+                        <FormControl>
+                          <Input
+                            autoComplete="current-password"
+                            placeholder="Old password" {...field}
+                            type="password"
+                            className="w-[350px]" />
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="new_password"
+                  render={({field}) => (
+                    <FormItem className="flex flex-col">
+                      <div className="flex flex-row items-center justify-between w-[40vw] mt-2">
+                        <FormLabel className="font-semibold text-base">{'New password'}</FormLabel>
+                        <FormControl>
+                          <Input
+                            autoComplete="new-password"
+                            placeholder="New password" {...field}
+                            type="password"
+                            className="w-[350px]" />
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="re_confirm_password"
+                  render={({field}) => (
+                    <FormItem className="flex flex-col">
+                      <div className="flex flex-row items-center justify-between w-[40vw] mt-2">
+                        <FormLabel className="font-semibold text-base">{'Confirm new password'}</FormLabel>
+                        <FormControl>
+                          <Input
+                            autoComplete="new-password"
+                            placeholder="Confirm new password" {...field}
+                            type="password"
+                            className="w-[350px]" />
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button className="mt-6">{'Update'}</Button>
+              </form>
+            </Form>
+          </div>
+
+          <div className="m-3 mt-12 border-b-[1px] p-2">
+            <p className="font-bold text-rose-500">{'Delete Account'}</p>
+          </div>
+
+          <div className="mx-3 p-2 space-y-4">
+            <p>{ACCOUNT.DELETE_DESCRIPTION}</p>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button className="bg-white text-red-400 hover:bg-rose-500 hover:text-white border border-rose-500">
+                  {'Delete your account'}
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>{'Are you absolutely sure?'}</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    {ACCOUNT.ALERT}
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>{'Cancel'}</AlertDialogCancel>
+                  <AlertDialogAction>{'Continue'}</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        </div>
       </div>
-
-      <div className="flex flex-col">
-        <div className="m-3 border-b-[1px] p-2">
-          <p className="font-semibold">{'Change Password'}</p>
-        </div>
-
-        <div className="mx-3 p-2 space-y-4">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit({onSubmit, onError})}>
-              {/* To support accesibility */}
-              <input hidden type="text" autoComplete="username" />
-
-              <FormField
-                control={form.control}
-                name="old_password"
-                render={({field}) => (
-                  <FormItem className="flex flex-col">
-                    <div className="flex flex-row items-center justify-between w-[40vw] mt-2">
-                      <FormLabel className="font-semibold text-base">{'Old password'}</FormLabel>
-                      <FormControl>
-                        <Input
-                          autoComplete="current-password"
-                          placeholder="Old password" {...field}
-                          type="password"
-                          className="w-[350px]" />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="new_password"
-                render={({field}) => (
-                  <FormItem className="flex flex-col">
-                    <div className="flex flex-row items-center justify-between w-[40vw] mt-2">
-                      <FormLabel className="font-semibold text-base">{'New password'}</FormLabel>
-                      <FormControl>
-                        <Input
-                          autoComplete="new-password"
-                          placeholder="New password" {...field}
-                          type="password"
-                          className="w-[350px]" />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="re_confirm_password"
-                render={({field}) => (
-                  <FormItem className="flex flex-col">
-                    <div className="flex flex-row items-center justify-between w-[40vw] mt-2">
-                      <FormLabel className="font-semibold text-base">{'Confirm new password'}</FormLabel>
-                      <FormControl>
-                        <Input
-                          autoComplete="new-password"
-                          placeholder="Confirm new password" {...field}
-                          type="password"
-                          className="w-[350px]" />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <Button className="mt-6">{'Update'}</Button>
-            </form>
-          </Form>
-        </div>
-
-        <div className="m-3 mt-12 border-b-[1px] p-2">
-          <p className="font-bold text-rose-500">{'Delete Account'}</p>
-        </div>
-
-        <div className="mx-3 p-2 space-y-4">
-          <p>{ACCOUNT.DELETE_DESCRIPTION}</p>
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button className="bg-white text-red-400 hover:bg-rose-500 hover:text-white border border-rose-500">
-                {'Delete your account'}
-              </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>{'Are you absolutely sure?'}</AlertDialogTitle>
-                <AlertDialogDescription>
-                  {ACCOUNT.ALERT}
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>{'Cancel'}</AlertDialogCancel>
-                <AlertDialogAction>{'Continue'}</AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        </div>
-      </div>
-    </div>
+    </OverlayScrollbarsComponent>
   );
 }
 
