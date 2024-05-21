3da4597e5bb6b6078f66aa60e992c8ec1aeebcaf
@S52100903 <52100903@student.tdtu.edu.vn>
2024-05-21 16:18:08 +0700

    feat: seeding event data

---
+++ src/features/dashboard/test/data/event.ts
@@ -0,0 +1,38 @@
+export const events = [
+    {
+        id: 1,
+        name: 'Fix UI for Dashboard',
+        dueTo: '2024-05-25'
+    },
+    {
+        id: 2,
+        name: 'International Children Day',
+        dueTo: '2024-06-01'
+    },
+    {
+        id: 3,
+        name: 'Someone Birthday',
+        dueTo: '2024-06-7'
+    },
+    {
+        id: 4,
+        name: 'Iterview Day',
+        dueTo: '2024-06-10'
+    },
+    {
+        id: 5,
+        name: 'Random Day',
+        dueTo: '2024-06-25'
+    },
+    {
+        id: 6,
+        name: 'Random Event',
+        dueTo: '2024-06-25'
+    },
+    {
+        id: 7,
+        name: 'Anh Kien Tac Co',
+        dueTo: '2024-05-25'
+    },
+
+]
