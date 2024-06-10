b9175199d8fa7a27abd47fd9b0300a61bab497fd
Mudoker <huuquoc7603@gmail.com>
2024-06-10 15:42:27 +0700

    ref: update eslint to match prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,53 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  // Environment settings
+  env: {
+    browser: true, // Enable browser environment
+    es2021: true, // Support ES2021 syntax
   },
-  'extends': [
-    'google',
-    'plugin:react/recommended',
+  // Extend ESLint configurations
+  extends: [
+    'eslint:recommended', // Use ESLint recommended rules
+    'plugin:react/recommended', // Use recommended React rules
+    'plugin:prettier/recommended', // Use recommended Prettier rules
+    'google', // Use Google style guide
   ],
-  'overrides': [
+  // Overrides for specific files
+  overrides: [
     {
-      'env': {
-        'node': true,
+      env: {
+        node: true, // Enable Node.js environment
       },
-      'files': [
-        '.eslintrc.{js,cjs}',
-      ],
-      'parserOptions': {
-        'sourceType': 'script',
+      files: ['.eslintrc.{js,cjs}'], // Apply to ESLint configuration files
+      parserOptions: {
+        sourceType: 'script', // Use script parser for configuration files
       },
     },
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+  // Parser options
+  parserOptions: {
+    ecmaVersion: 'latest', // Support latest ECMAScript features
+    sourceType: 'module', // Use ECMAScript modules
   },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-    'react/react-in-jsx-scope': 'off',
-    'require-jsdoc': 0,
-    'max-len': 'off',
+  // Plugins used
+  plugins: ['react', 'prettier'], // Use React and Prettier plugins
+  // Rules configuration
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed for React 17+
+    'require-jsdoc': 'off', // Turn off Google style doc requirement
+    'max-len': 'off', // Turn off max line length
+    'prettier/prettier': 'error', // Treat prettier errors as ESLint errors
     'no-restricted-imports': [
       'error',
       {
-        patterns: ['@/features/*/*'],
+        patterns: ['@/features/*/*'], // Restrict certain import patterns
       },
     ],
+    'react/prop-types': 'off', // Turn off prop-types requirement
+  },
+  // Settings
+  settings: {
+    react: {
+      version: 'detect', // Auto-detect the react version
+    },
   },
 };
