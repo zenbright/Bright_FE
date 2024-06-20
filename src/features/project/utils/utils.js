3390a6fb92c83752bf94889e7984f58c28908676
Mudoker <huuquoc7603@gmail.com>
2024-06-21 00:25:24 +0700

    refactor: Remove commented out code and fix eslint warnings

--- src/features/project/utils/utils.js
+++ src/features/project/utils/utils.js
@@ -1,65 +1,65 @@
 import tinycolor from 'tinycolor2';
 
 export const neonColorCreator = (bgColor = '') => {
-    if (bgColor === '') {
-        console.error('Background color is required');
-        return;
-    }
+  if (bgColor === '') {
+    console.error('Background color is required');
+    return;
+  }
 
-    const foregroundColor = tinycolor(bgColor).lighten(50).toString();
-    return foregroundColor;
-}
+  const foregroundColor = tinycolor(bgColor).lighten(50).toString();
+  return foregroundColor;
+};
 
 export function formatDate(date) {
-    // Array of month names
-    const monthNames = [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec',
-    ];
+  // Array of month names
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
 
-    // Get month, day, and year from the Date object
-    const month = monthNames[date.getMonth()];
-    const day = date.getDate();
-    const year = date.getFullYear();
+  // Get month, day, and year from the Date object
+  const month = monthNames[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
 
-    // Format the date string
-    const formattedDate = `${month} ${day}, ${year}`;
+  // Format the date string
+  const formattedDate = `${month} ${day}, ${year}`;
 
-    return formattedDate;
+  return formattedDate;
 }
 
 export function timeAgo(date) {
-    const seconds = Math.floor((new Date() - date) / 1000);
-    let interval = Math.floor(seconds / 31536000);
+  const seconds = Math.floor((new Date() - date) / 1000);
+  let interval = Math.floor(seconds / 31536000);
 
-    if (interval >= 1) {
-        return interval + ' year' + (interval === 1 ? '' : 's') + ' ago';
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval >= 1) {
-        return interval + ' month' + (interval === 1 ? '' : 's') + ' ago';
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval >= 1) {
-        return interval + ' day' + (interval === 1 ? '' : 's') + ' ago';
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval >= 1) {
-        return interval + ' hour' + (interval === 1 ? '' : 's') + ' ago';
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval >= 1) {
-        return interval + ' minute' + (interval === 1 ? '' : 's') + ' ago';
-    }
-    return 'just now';
+  if (interval >= 1) {
+    return interval + ' year' + (interval === 1 ? '' : 's') + ' ago';
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval >= 1) {
+    return interval + ' month' + (interval === 1 ? '' : 's') + ' ago';
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) {
+    return interval + ' day' + (interval === 1 ? '' : 's') + ' ago';
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval >= 1) {
+    return interval + ' hour' + (interval === 1 ? '' : 's') + ' ago';
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval >= 1) {
+    return interval + ' minute' + (interval === 1 ? '' : 's') + ' ago';
+  }
+  return 'just now';
 }
