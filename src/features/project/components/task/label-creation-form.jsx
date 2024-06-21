7071f2e17b8c1f79be7129b0dab275f8884e3005
Mudoker <huuquoc7603@gmail.com>
2024-06-21 17:53:07 +0700

    chore: clean code

--- src/features/project/components/task/label-creation-form.jsx
+++ src/features/project/components/task/label-creation-form.jsx
@@ -7,7 +7,6 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
