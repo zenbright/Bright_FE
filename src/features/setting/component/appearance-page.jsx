a270596a9772e30ddbf3ac248eb74e38a58ec3b8
Mudoker <huuquoc7603@gmail.com>
2024-06-21 18:29:24 +0700

    chore: add custom slider

--- src/features/setting/component/appearance-page.jsx
+++ src/features/setting/component/appearance-page.jsx
@@ -1,3 +1,5 @@
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
+
 import catImage from '../asset/cat.jpg';
 import themes from '../test/data/themes';
 import Theme from './app-theme';
@@ -5,24 +7,30 @@ import Theme from './app-theme';
 function Appearance() {
   return (
     <div className="container-ns flex flex-col w-[76vw] overflow-auto">
-      <div className="mx-3 text-lg font-bold top-0 p-2 border-b-[1px] border-slate-300 group sticky bg-white">
-        {'Appearance'}
-      </div>
-      <div className="flex flex-col m-4">
-        <div className="border-b-[1px] py-2 mb-3">
-          <p className="font-semibold text-md">{'Theme Preferences'}</p>
+      <OverlayScrollbarsComponent
+        element="div"
+        options={{ scrollbars: { autoHide: 'move' } }}
+        defer
+      >
+        <div className="mx-3 text-lg font-bold top-0 p-2 border-b-[1px] border-slate-300 group sticky bg-white">
+          {'Appearance'}
         </div>
-        <p>{'Select a single theme'}</p>
-        <div className="my-5">
-          <form className="grid grid-cols-3 gap-4">
-            {themes.map((theme, index) => (
-              <label key={index} className="rounded-md w-fit cursor-pointer">
-                <Theme name={theme} image={catImage}></Theme>
-              </label>
-            ))}
-          </form>
+        <div className="flex flex-col m-4">
+          <div className="border-b-[1px] py-2 mb-3">
+            <p className="font-semibold text-md">{'Theme Preferences'}</p>
+          </div>
+          <p>{'Select a single theme'}</p>
+          <div className="my-5">
+            <form className="grid grid-cols-3 gap-4">
+              {themes.map((theme, index) => (
+                <label key={index} className="rounded-md w-fit cursor-pointer">
+                  <Theme name={theme} image={catImage}></Theme>
+                </label>
+              ))}
+            </form>
+          </div>
         </div>
-      </div>
+      </OverlayScrollbarsComponent>
     </div>
   );
 }
