ea7c0623c3d4328c39132064b1b1355be8354bdb
Mudoker <huuquoc7603@gmail.com>
2024-06-21 18:30:06 +0700

    chore: Disable inavailable themes in app-theme component

--- src/features/setting/component/app-theme.jsx
+++ src/features/setting/component/app-theme.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 
 function Theme({ name, image }) {
+  const isDisabled = name !== 'Light default' && name !== 'Dark default';
+
   return (
     <div className="rounded-md w-full border-[2px] cursor-pointer">
       <img src={image} alt="" className="w-[300px] h-[180px] object-fill" />
       <div className="flex flex-row p-2 gap-2">
-        <input type="radio" name="theme" value={name} />
+        <input type="radio" name="theme" value={name} disabled={isDisabled} />
         <p>{name}</p>
       </div>
     </div>
--- src/features/setting/test/data/themes.js
+++ src/features/setting/test/data/themes.js
@@ -1,11 +1,9 @@
 const themes = [
   'Light default',
   'Light high contrast',
-  'Light Protaponia & Deuteranopia',
   'Light Tritanopia',
   'Dark default',
   'Dark high contrast',
-  'Dark Protaponia & Deuteranopia',
   'Dark Tritanopia',
   'Dark dimmed',
 ];
