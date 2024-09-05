658545232448f9190ca19ef1993a6402d459562a
DarkChan <buihohaidang@gmail.com>
2024-09-05 08:27:27 +0700

    refactor: Add lazy loading for images in landing page components

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App.jsx';
-import { store } from './config/redux/store.ts';
+import { store } from './config/redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
