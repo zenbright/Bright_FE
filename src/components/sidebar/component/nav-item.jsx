22d7875a7f6b42d28c8eb2eb4a24d8181cef57cd
quietfriend03 <kientrungplus@example.com>
2024-09-09 09:38:48 +0700

    refactor: re-align the component and add navigation to setting page

--- src/components/sidebar/component/nav-item.jsx
+++ src/components/sidebar/component/nav-item.jsx
@@ -13,7 +13,9 @@ function NavItem({ text, children, onClick, select = false }) {
           className={`flex group-hover:w-[90%] h-[90%] rounded-md w-fit text-sm
           ${select ? 'bg-foreground fill-white text-white duration-0' : 'bg-background group-hover/item:bg-hovering'}`}
         >
-          <div className=" dropdown-content flex justify-center items-center px-2 py-1.5">
+          <div className={`dropdown-content flex justify-center items-center px-2 py-1.5
+            ${select ? 'fill-background duration-200' : 'fill-foreground group-hover/item:fill-black'}`}
+          >
             {children}
           </div>
           <div
--- src/components/sidebar/component/user-image-name.jsx
+++ src/components/sidebar/component/user-image-name.jsx
@@ -1,28 +1,31 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
 function UserImageName({ text, image }) {
   return (
-    <div
-      id={text}
-      className="pl-3 dropdown flex w-full gap-6 transition-all relative h-12"
-    >
-      <div className="cursor-pointer h-12 w-[100%] flex items-center align-middle hover/item:bg-slate-300 op hover/item:bg-opacity-20 hover/item:w-[90%] rounded-md group is-hovered mt-4">
-        <div className="fixed justify-center items-center dropdown-content w-[6vw] place-content-center flex transition-all">
-          <Avatar className="h-10 w-10 mr-8 rounded-full border-2 cursor-pointer">
-            <AvatarImage src={image} alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </div>
-        <div
-          className="font-medium relative text-sm md:text-sm opacity-0 duration-200 group-hover:opacity-100
+    <NavLink to={`/user/settings/edit-profile`}>
+      <div
+        id={text}
+        className="pl-3 dropdown flex w-full gap-6 transition-all relative h-12"
+      >
+        <div className="cursor-pointer h-12 w-[100%] flex items-center align-middle hover/item:bg-slate-300 op hover/item:bg-opacity-20 hover/item:w-[90%] rounded-md group is-hovered mt-4">
+          <div className="fixed justify-center items-center dropdown-content w-[6vw] place-content-center flex transition-all">
+            <Avatar className="h-10 w-10 mr-8 rounded-full border-2 cursor-pointer">
+              <AvatarImage src={image} alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <div
+            className="font-medium relative text-sm md:text-sm opacity-0 duration-200 group-hover:opacity-100
                 transition-all transition-300 overflow-hidden invisible group-hover:visible w-0 left-[5vw] group-hover:w-full flex items-center
             "
-        >
-          {text}
+          >
+            {text}
+          </div>
         </div>
       </div>
-    </div>
+    </NavLink>
   );
 }
 
