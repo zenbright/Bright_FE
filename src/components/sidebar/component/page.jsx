26f16e940ec69155d667123d645844ec124d5263
quietfriend03 <kientrungplus@example.com>
2024-09-23 10:36:15 +0700

    refactor: fixing dark mode for sidebar

--- src/components/sidebar/component/page.jsx
+++ src/components/sidebar/component/page.jsx
@@ -20,11 +20,11 @@ function Sidebar() {
 
   return (
     <div className="flex flex-row container-ns">
-      <div className="relative group w-[5.5vw] hover:w-[18vw] transition-al bg-black duration-200 bg-white border-r-2 pt-2 h-screen flex flex-col items-center">
+      <div className="relative group w-[5.5vw] hover:w-[18vw] transition-al duration-200 bg-background border-r-2 pt-2 h-screen flex flex-col items-center">
         {/* User Information */}
         <UserImageName text={'Username'} image={''} />
 
-        <div className="line mt-2 mb-3 p-[0.5px] w-[0vw] opacity-0 group-hover:opacity-100 bg-slate-400 overflow-hidden" />
+        <div className="line mt-2 mb-3 p-[0.5px] w-[0vw] opacity-0 group-hover:opacity-100 bg-background overflow-hidden" />
 
         {/* Navigation Content */}
         <div className="flex flex-col relative w-full h-screen space-y-4">
@@ -34,7 +34,7 @@ function Sidebar() {
             select={selectedItem === 'Dashboard'}
           >
             <>
-              <DashboardIcon className="w-5 h-5 hover:fill-white" />
+              <DashboardIcon className="w-5 h-5" />
               <div className="grandchild-content absolute left-[18vw] top-[-77px] h-screen">
                 <ProjectDashboard />
               </div>
@@ -46,7 +46,7 @@ function Sidebar() {
             onClick={handleNavClick}
             select={selectedItem === 'Notification'}
           >
-            <NotificationIcon className="w-5 h-5 hover:fill-white" />
+            <NotificationIcon className="w-5 h-5" />
           </NavItem>
 
           <NavItem
@@ -54,14 +54,14 @@ function Sidebar() {
             onClick={handleNavClick}
             select={selectedItem === 'Settings'}
           >
-            <SettingIcon className="w-5 h-5 hover:fill-white" />
+            <SettingIcon className="w-5 h-5" />
           </NavItem>
         </div>
 
         {/* Logout button */}
         <div className="w-full pb-1">
           <LogoutButton text={'Logout'}>
-            <LogoutIcon className="w-5 h-5 hover:fill-white" />
+            <LogoutIcon className="w-5 h-5" />
           </LogoutButton>
         </div>
       </div>
