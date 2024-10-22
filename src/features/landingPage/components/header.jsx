546c373b595739e04baf88285bb3e5255d1690d0
Mudoker <huuquoc7603@gmail.com>
2024-10-22 12:54:21 +0700

    Refactor: Update landing page header styling and navigation menu group

--- src/features/landingPage/components/header.jsx
+++ src/features/landingPage/components/header.jsx
@@ -1,7 +1,5 @@
 import BrightLogo from '@/assets/images/app-logo/logomini-light.svg';
 import { Button } from '@/components/ui/button';
-import { HoverBorderGradient } from '@components/ui/hover-border-gradient';
-import { Heart } from 'lucide-react';
 import { Zap } from 'lucide-react';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
@@ -12,17 +10,18 @@ export const Header = React.forwardRef((_, ref) => {
   return (
     <div
       ref={ref}
-      className="sticky top-0 z-50 bg-transparent px-4 py-2 text-white"
+      className="sticky top-0 z-50 bg-none px-4 py-2 text-white"
     >
-      <div className="mb-2 grid h-20 grid-cols-3 items-center bg-black px-4">
+      <div className="mb-2 grid h-20 grid-cols-3 items-center px-4">
         <div className="flex h-12 items-center">
           <img src={BrightLogo} className="h-40 w-40 hover:cursor-pointer" />
         </div>
 
         <div className="flex items-center justify-center gap-3">
           <NavigationMenuGroup />
+
           <Button className="text-white" variant="link">
-            Pricing
+            {'Pricing'}
           </Button>
           <Button
             className="text-white"
@@ -31,15 +30,15 @@ export const Header = React.forwardRef((_, ref) => {
               window.open('https://github.com/zenbright/');
             }}
           >
-            GitHub
+            {'GitHub'}
           </Button>
         </div>
 
         <div className="flex items-center justify-end gap-3">
           <NavLink to={'/auth'}>
-            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1.5px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-10">
+              <span className="absolute inset-[-500%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                 {'Join the Wishlist'} <Zap className="ml-1.5 h-4 w-4" />
               </span>
             </button>
--- src/features/landingPage/components/navigation-menu-group.tsx
+++ src/features/landingPage/components/navigation-menu-group.tsx
@@ -56,59 +56,57 @@ ListItem.displayName = 'ListItem';
 
 export const NavigationMenuGroup: React.FC = () => {
   return (
-    <div className="ml-4 list-none">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="bg-black">{"Getting started"}</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
-                  <NavigationMenuLink asChild>
-                    <a
-                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                      href="/"
-                    >
-                      <img src={BrightLogo} alt="Bright Logo" className="" />
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        {"Zen Bright"}
-                      </div>
-                      <p className="text-sm leading-tight text-muted-foreground">
-                        {"A project management and coding platform with AI insights, GitHub integration, and real-time collaboration to boost productivity."}
-                      </p>
-                    </a>
-                  </NavigationMenuLink>
-                </li>
-                <ListItem href="/docs" title="Introduction">
-                  {"Tools to organize tasks, deadlines, and team workflows effectively."}
-                </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  {"Steps to install necessary packages and configure our product."}
-                </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Sponsor">
-                  {"Support our development and help us create more features for the community."}
-                </ListItem>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="bg-black">Features</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                {components.map((component) => (
-                  <ListItem
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
+    <NavigationMenu >
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="bg-transparent">{"Getting started"}</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
                   >
-                    {component.description}
-                  </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
+                    <img src={BrightLogo} alt="Bright Logo" className='' />
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      {"Zen Bright"}
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      {"A project management and coding platform with AI insights, GitHub integration, and real-time collaboration to boost productivity."}
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                {"Tools to organize tasks, deadlines, and team workflows effectively."}
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                {"Steps to install necessary packages and configure our product."}
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Sponsor">
+                {"Support our development and help us create more features for the community."}
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="bg-transparent">Features</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
